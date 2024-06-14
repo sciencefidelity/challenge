a07bf91a3b882f2df8ca6a19762fa452ceb2d1ac
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-06-15 00:19:11 +0100

    feat: solve 945

---
+++ src/pr_0945.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_increment_for_unique(nums: Vec<i32>) -> i32 {
+        let mut min_increments = 0;
+        let max = nums.iter().max().unwrap();
+        let mut frequency_count = vec![0; nums.len() + *max as usize];
+
+        for val in nums {
+            frequency_count[val as usize] += 1;
+        }
+
+        for i in 0..frequency_count.len() {
+            if frequency_count[i] <= 1 {
+                continue;
+            }
+            let duplicates = frequency_count[i] - 1;
+            frequency_count[i + 1] += duplicates;
+            frequency_count[i] = 1;
+            min_increments += duplicates;
+        }
+        min_increments
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::min_increment_for_unique(vec![1, 2, 2]), 1);
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(
+            Solution::min_increment_for_unique(vec![3, 2, 1, 2, 1, 7]),
+            6
+        );
+    }
+}
