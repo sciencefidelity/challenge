41972105d754e6527bf78b3958bd3108fcf140e9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-07-06 20:22:19 +0100

    feat: solve 2582

---
+++ src/pr_100336.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn number_of_alternating_groups(colors: Vec<i32>) -> i32 {
+        let mut groups = 0;
+        let first = colors[0];
+        let last = colors[colors.len() - 1];
+        for (i, color) in colors.iter().enumerate() {
+            let before = if i == 0 { last } else { colors[i - 1] };
+            let after = if i == colors.len() - 1 {
+                first
+            } else {
+                colors[i + 1]
+            };
+            if before != *color && after != *color {
+                groups += 1;
+            }
+        }
+
+        groups
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::number_of_alternating_groups(vec![1, 1, 1]), 0);
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(
+            Solution::number_of_alternating_groups(vec![0, 1, 0, 0, 1]),
+            3
+        );
+    }
+}
---
+++ src/pr_2582.rs
@@ -0,0 +1,33 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
+        let full_rounds = time / (n - 1);
+        let extra_time = time % (n - 1);
+        if full_rounds % 2 == 0 {
+            extra_time + 1
+        } else {
+            n - extra_time
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::pass_the_pillow(4, 5), 2);
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::pass_the_pillow(3, 2), 3);
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(Solution::pass_the_pillow(9, 4), 5);
+    }
+}
