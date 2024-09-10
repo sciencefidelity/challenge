b5bdfe868a66067772b417d8977bf87259d59b28
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-09-10 10:22:16 +0100

    refactor: optimise 875

--- src/pr_0875.rs
+++ src/pr_0875.rs
@@ -3,26 +3,26 @@ pub struct Solution;
 impl Solution {
     #[allow(clippy::needless_pass_by_value)]
     pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+        let can_finish = |k| {
+            let mut hours = h;
+            for pile in &piles {
+                hours -= (pile + k - 1) / k;
+                if hours < 0 {
+                    break;
+                }
+            }
+            hours >= 0
+        };
         let (mut l, mut r) = (1, *piles.iter().max().unwrap());
         while l < r {
             let k = (l + r) / 2;
-            if Self::can_finish(k, h, &piles) {
+            if can_finish(k) {
                 r = k;
             } else {
                 l = k + 1;
             }
         }
         l
-    }
-
-    fn can_finish(k: i32, mut h: i32, piles: &[i32]) -> bool {
-        for pile in piles {
-            h -= (pile + k - 1) / k;
-            if h < 0 {
-                break;
-            }
-        }
-        h >= 0
     }
 }
 
