8d41b3da762deec407aded844eb3fcfab7e0aa08
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-01-08 00:08:03 +0000

    feat: solve 1408

---
+++ src/pr_1408.rs
@@ -0,0 +1,11 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn string_matching(mut words: Vec<String>) -> Vec<String> {
+        let joined = words.join(" ");
+        words
+            .into_iter()
+            .filter(|word| joined.matches(word).nth(1).is_some())
+            .collect()
+    }
+}
